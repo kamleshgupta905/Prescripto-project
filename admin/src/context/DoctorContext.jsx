98544e031ae74d2f644da0842b393da9a8c623ee
Kamlesh Gupta <kamleshg9569@gmail.com>
2025-08-03 18:43:50 +0530

    Update DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -7,7 +7,7 @@ export const DoctorContext = createContext()
 
 const DoctorContextProvider = (props) => {
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = import.meta.env.https://prescripto-project-three.vercel.app
 
     const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
     const [appointments, setAppointments] = useState([])
